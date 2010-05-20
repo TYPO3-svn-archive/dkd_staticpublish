6400fc01747bbccc0157de2036928c0d2e8b7843
thorsten <thorsten@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-20 12:10:01 +0000

    * Removed unused module function from Web > Functions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dkd_staticpublish/trunk@33330 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -5,17 +5,7 @@
 
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-if (TYPO3_MODE=='BE')	{
-	t3lib_extMgm::insertModuleFunction(
-		'web_func',		
-		'tx_dkdstaticpublish_modfunc1',
-		t3lib_extMgm::extPath($_EXTKEY).'modfunc1/class.tx_dkdstaticpublish_modfunc1.php',
-		'LLL:EXT:dkd_staticpublish/locallang_db.php:moduleFunction.tx_dkdstaticpublish_modfunc1',
-		'wiz'	
-	);
-}
-
-
+	// add context menu entry
 if (TYPO3_MODE=='BE')	{
 	$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][]=array(
 		'name' => 'tx_dkdstaticpublish_cm',
